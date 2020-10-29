43688730bb9390df555a34dbe1b9f5b7b9314276
Shreya Sridhar <shreyamohak@outlook.com>
2020-10-29 23:29:40 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,6 +15,14 @@ def loop_message_n_times(message, limit)
   end
 end
 
+def output_array(array)
+  count = 0
+  while count < array.length do
+    puts array[count]
+    count += 1
+  end
+end
+
 
 
 
